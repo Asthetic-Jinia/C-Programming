582040850fc110b0cc6600fad310309f4eaa5b26
Asthetic-Jinia <bhaumikjinia@gmail.com>
2025-02-05 11:56:56 +0530

    x

--- SwapWithExtraVariable.c
+++ SwapWithExtraVariable.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main(){
-    int x,y,z,X,Y;
+    int x,y,z;
     printf("Enter the value of x:");
     scanf("%d",&x);
     printf("Enter the value of y:");
     scanf("%d",&y);
-    z=x+y;
-    X=z-x;
-    Y=z-y;
-    printf("x=%d\n",X);
-    printf("y=%d",Y);
+    z=x;
+    x=y;
+    y=z;
+    printf("x=%d\n",x);
+    printf("y=%d",y);
     return 0;
 
 }
