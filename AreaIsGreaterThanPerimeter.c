bbf5b821ec9970f85fd2a05586be843e2e43f0fc
Asthetic-Jinia <bhaumikjinia@gmail.com>
2025-02-08 09:23:13 +0530

    area is less than its perimeter

--- AreaIsGreaterThanPerimeter.c
+++ AreaIsGreaterThanPerimeter.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 int main(){
-    int l,b;
-    
+    int l,b,area,perimeter;
+    printf("Enter the value of l and b");
+    scanf("%d%d",&l,&b);
+    area=l*b;
+    perimeter=2*(l+b);
+    if(area>perimeter){
+        printf("Area is greater than its perimeter");
+    }else{
+        printf("area is less than its parameter");
+    }
+    return 0;
+
 }
